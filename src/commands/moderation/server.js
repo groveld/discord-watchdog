82f7bc4f2d83b079fbb08d760f5205bf85d4ddfa
Martin Groeneveld <martin@groveld.com>
2020-07-13 20:39:43 +0200

    Added server info command and fixed CI image

---
+++ src/commands/moderation/server.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require('discord.js');
+
+module.exports = {
+  name: 'server',
+  description: 'server info',
+  execute: async (message) => {
+    const embed = new MessageEmbed()
+      .setColor(0x00AE86)
+      .setTitle('Server Info')
+      .setThumbnail(message.guild.iconURL)
+      .addField(':arrow_right: Name', message.guild.name, true)
+      .addField(':arrow_right: ID', message.guild.id, true)
+      .addField(':arrow_right: Region', message.guild.region.toUpperCase(), true)
+      .addField(':arrow_right: Creation Date', message.guild.createdAt.toDateString(), true)
+      .addField(':arrow_right: Owner', message.guild.owner.user.tag, true)
+      .addField(':arrow_right: Members', message.guild.memberCount, true)
+      .addField(':arrow_right: Roles', message.guild.roles.cache.map(role => role.toString()).join(' **|** '), true)
+      .addField(':arrow_right: Categories', message.guild.channels.cache.filter(channel => channel.type === 'category').map(category => category.toString()).join(' **|** '), true)
+      .addField(':arrow_right: Channels', message.guild.channels.cache.filter(channel => channel.type !== 'category').map(channel => channel.toString()).join(' **|** '), true);
+
+    return message.channel.send(embed);
+  },
+};
