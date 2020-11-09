185ec20da67adbfecbab02e599cc65d046f05571
Martin Groeneveld <martin@groveld.com>
2020-11-09 19:28:25 +0100

    fixed linting issues

--- src/commands/debug/args-info.js
+++ src/commands/debug/args-info.js
@@ -1,8 +1,8 @@
 module.exports = {
   name: 'args-info',
   description: 'Information about the arguments provided.',
-args:true,
-  execute(message, args){
+  args:true,
+  execute(message, args) {
     if (args[0] === 'foo') {
       return message.channel.send('bar');
     }
--- src/events/message.js
+++ src/events/message.js
@@ -26,9 +26,8 @@ module.exports = (client, message) => {
     return message.channel.send(reply);
   }
 
-  if (message.mentions.users.array().size >= 25 ||
-      message.mentions.members.array().size >= 25) {
-  	message.member.ban({reason: 'Auto Moderation: Mentioned more than 25 members in a message.'})
+  if (message.mentions.users.array().size >= 25 || message.mentions.members.array().size >= 25) {
+    message.member.ban({ reason: 'Auto Moderation: Mentioned more than 25 members in a message.' });
   }
 
   const cooldowns = new Collection();
