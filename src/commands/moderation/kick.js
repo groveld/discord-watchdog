75669d4e0d0d1f5ea0d43e67812e068ef175c2f2
Martin Groeneveld <martin@groveld.com>
2020-11-09 23:02:07 +0100

    fixed kick command (2)?

--- src/commands/moderation/kick.js
+++ src/commands/moderation/kick.js
@@ -7,13 +7,22 @@ module.exports = {
       return message.reply('you need to tag a user in order to kick them!');
     }
 
-    const taggedUser = message.mentions.users.first();
+    const user = message.mentions.users.first();
+    const member = message.guild.member(user);
 
-    taggedUser.kick('Optional reason that will display in the audit logs')
+    member
+      .kick('Optional reason that will display in the audit logs')
       .then(() => {
-        message.reply(`Successfully kicked ${taggedUser.username}!`);
-      }).catch(() => {
-        message.reply(`I was unable to kick ${taggedUser.username}. Check if their roles are higher then mine or if they have administrative permissions!`);
+        // We let the message author know we were able to kick the person
+        message.reply(`Successfully kicked ${user.username}`);
+      })
+      .catch(err => {
+        // An error happened
+        // This is generally due to the bot not being able to kick the member,
+        // either due to missing permissions or role hierarchy
+        message.reply(`I was unable to kick ${user.username}. Check if their roles are higher then mine or if they have administrative permissions!`);
+        // Log the error
+        console.error(err);
       });
   },
 };
